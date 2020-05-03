206255c285c4a2322d52a2ee1b046ed501a57fec
russellmcintire20 <McIntires@McIntires-MBP.lan>
2020-05-03 18:36:10 -0500

    changing get response

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-app.get('/', (req, res)=> { res.send(database.users) })
+app.get('/', (req, res)=> { res.send('it is working!') })
 app.post('/signin', signin.handleSignin(db, bcrypt))
 app.post('/register', register.handleRegister(db, bcrypt, saltRounds))
 app.get('/profile/:id', profile.handleProfileGet(db))
