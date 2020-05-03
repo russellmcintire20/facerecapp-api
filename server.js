b436bf05acae25cd87a410ffd85fc90f2b2c9d77
russellmcintire20 <McIntires@McIntires-MBP.lan>
2020-05-03 18:57:47 -0500

    adding database connection

--- server.js
+++ server.js
@@ -12,10 +12,8 @@ const image = require('./controllers/image');
 const db = knex({
   client: 'pg',
   connection: {
-    host : 'postgresql-aerodynamic-45721',
-    user : 'McIntires',
-    password : '',
-    database : 'facerec-app'
+    host : process.env.DATABASE_URL,
+    ssl: true,
   }
 });
 
